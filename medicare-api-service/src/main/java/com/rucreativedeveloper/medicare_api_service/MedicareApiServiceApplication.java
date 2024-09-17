602708409523509b853b10452e5ac593dd3c3c20
Ravindu Wanshanatha <63859006+ravindu568@users.noreply.github.com>
2024-09-17 23:31:12 +0530

    layered architecture folder structure created

--- medicare-api-service/src/main/java/com/rucreativedeveloper/medicare_api_service/MedicareApiServiceApplication.java
+++ medicare-api-service/src/main/java/com/rucreativedeveloper/medicare_api_service/MedicareApiServiceApplication.java
@@ -1,13 +1,20 @@
 package com.rucreativedeveloper.medicare_api_service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MedicareApiServiceApplication {
+@RequiredArgsConstructor
+public class MedicareApiServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedicareApiServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
