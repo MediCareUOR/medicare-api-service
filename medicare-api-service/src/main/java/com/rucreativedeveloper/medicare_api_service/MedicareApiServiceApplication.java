a5e7a76708ab24bc5da9af37ff25a40f3dad4a7d
Ravindu Wanshanatha <63859006+ravindu568@users.noreply.github.com>
2024-09-17 09:58:31 +0530

    initial commit

---
+++ medicare-api-service/src/main/java/com/rucreativedeveloper/medicare_api_service/MedicareApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rucreativedeveloper.medicare_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareApiServiceApplication.class, args);
+	}
+
+}
---
+++ medicare-api-service/src/main/java/com/rucreativedeveloper/medicare_api_service/entity/SystemUser.java
@@ -0,0 +1,40 @@
+package com.rucreativedeveloper.medicare_api_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity(name="system_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SystemUser {
+    @Id
+    @Column(name="user_id", length = 80)
+    private String userId;
+    @Column(name="username", length = 100, unique = true)
+    private String username;// email
+    
+    private String password;
+
+    @Column(name="is_account_non_expired", columnDefinition = "TINYINT")
+    private boolean isAccountNonExpired;
+    @Column(name="is_account_non_locked", columnDefinition = "TINYINT")
+    private boolean isAccountNonLocked;
+    @Column(name="is_credentials_non_expired", columnDefinition = "TINYINT")
+    private boolean isCredentialsNonExpired;
+    @Column(name="is_enable", columnDefinition = "TINYINT")
+    private boolean isEnabled;
+
+    @ManyToMany
+    @JoinTable(name="user_role_detail",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name="role_id"))
+    private Set<UserRole> roles;
+
+
+
+}
